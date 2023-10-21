48e89b7744e6b253720eb5756686deac9cd1b486
Harsh Sharma <harshsharma0828@gmail.com>
2023-10-21 16:22:29 +0530

    Added some features in navbar

--- src/components/topnavbar.js
+++ src/components/topnavbar.js
@@ -15,14 +15,14 @@ export default function Navbar() {
     return (
         <div className='vsm:max-sm:flex flex justify-between h-[50px] mx-[4rem]'>
             <div className='vsm:max-sm: text-xl font-extrabold mt-2'>
-                <h1 className=' font-black font-serif'>KARIGAR</h1>
+                <h1 className=' font-black font-serif hover:cursor-pointer'>KARIGAR</h1>
             </div>
             <div className='vsm:max-sm:hidden flex gap-10 mt-3 opacity-70'>
-                <p>For you</p>
-                <p>Discover</p>
-                <p>Hire</p>
-                <p>Assests</p>
-                <p>Jobs</p>
+                <p className="hover:cursor-pointer">For you</p>
+                <p className="hover:cursor-pointer">Discover</p>
+                <p className="hover:cursor-pointer">Hire</p>
+                <p className="hover:cursor-pointer">Assests</p>
+                <p className="hover:cursor-pointer">Jobs</p>
             </div>
             <div className='vsm:max-sm:absolute vsm:max-sm:h-8 vsm:max-sm:text-lg vsm:max-sm:font-extrabold flex gap-4 mt-2'>
                 <div className='flex justify-between h-8 gap-6 font-bold text-white'>
